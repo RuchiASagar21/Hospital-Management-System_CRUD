5a597dc1fbacc9315246de32259ea35f470aa28b
RuchiASagar21 <113360969+RuchiASagar21@users.noreply.github.com>
2022-09-29 16:57:45 -0400

    Add files via upload

---
+++ Assignment_1/src/directory/Emp_data.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package directory;
+
+/**
+ *
+ * @author Ruchi Anand Sagar
+ */
+public class Emp_data {
+    private String name;
+    private int EMP_ID;
+    private int Age;
+    private String Gender;
+    private int Date;
+    private int level;
+    private String Team_Info;
+    private String Position_Title;
+    private int Phone_No;
+    private String Email_Add;
+    private String EMP_Photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEMP_ID() {
+        return EMP_ID;
+    }
+
+    public void setEMP_ID(int EMP_ID) {
+        this.EMP_ID = EMP_ID;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public int getDate() {
+        return Date;
+    }
+
+    public void setDate(int Date) {
+        this.Date = Date;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getTeam_Info() {
+        return Team_Info;
+    }
+
+    public void setTeam_Info(String Team_Info) {
+        this.Team_Info = Team_Info;
+    }
+
+    public String getPosition_Title() {
+        return Position_Title;
+    }
+
+    public void setPosition_Title(String Position_Title) {
+        this.Position_Title = Position_Title;
+    }
+
+    public int getPhone_No() {
+        return Phone_No;
+    }
+
+    public void setPhone_No(int Phone_No) {
+        this.Phone_No = Phone_No;
+    }
+
+    public String getEmail_Add() {
+        return Email_Add;
+    }
+
+    public void setEmail_Add(String Email_Add) {
+        this.Email_Add = Email_Add;
+    }
+
+    public String getEMP_Photo() {
+        return EMP_Photo;
+    }
+
+    public void setEMP_Photo(String EMP_Photo) {
+        this.EMP_Photo = EMP_Photo;
+    }
+    
+    
+    
+}
